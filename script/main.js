d4880395c8081ecfb3ec5982723b06c7539071dd
Mohamed <6416912-mohamed_ahmed98@users.noreply.gitlab.com>
2021-03-22 03:49:03 +0200

    version 1.4.0 commit new style

--- script/main.js
+++ script/main.js
@@ -1,10 +1,12 @@
-new Vue({
+var vm = new Vue({
     el: '#app',
     data: {
         ProjectName: 'بــسم الله الرحمن الرحيم ',
         Ayah: '',
         ayahApis: 'https://api.alquran.cloud/ayah/',
         surahName: '',
+        juzNumber: '',
+        revelationType: '',
         flag: false
     },
     methods: {
@@ -13,12 +15,35 @@ new Vue({
         },
         getAyah: async function() {
             let response = await fetch(this.ayahApis + this.randomNum() + "/ar.asad")
-            let data = await response.json();
-            this.Ayah = await data.data.text
-            this.surahName = await data.data.surah.name
-            this.flag = true
+            let responseData = await response.json();
+            this.Ayah = await responseData.data.text
+            this.surahName = await responseData.data.surah.name
+            this.juzNumber = await responseData.data.juz
+            this.revelationType = await responseData.data.revelationType
+            console.log(responseData.data)
         }
 
 
+    },
+    computed: {
+        revarseName: function() {
+            return
+        }
+    },
+    watch: {
+        surahName: function() {
+            this.flag = true
+        }
+
+    },
+    filters: {
+        currency: function(value) {
+            var formatter = Intl.NumberFormat('ar-EG');
+            return formatter.format(value);
+        }
     }
+})
+
+window.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
 })
